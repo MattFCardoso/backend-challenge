7c114400d8b5d74816d2c06114b587e6224b11fd
MattFC <matheus.fcardoso@hotmail.com>
2024-11-09 15:18:38 -0300

    [feat] create transaction entity

---
+++ src/main/java/com/challange/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,30 @@
+package com.challange.picpay.domain.transaction;
+
+import com.challange.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "transactions")
+@Table(name = "transaction")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long transactionId;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+    private LocalDateTime timestamp;
+}
